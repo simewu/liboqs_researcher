c104fb2e6f0341f4fc751f0f527d31db3c8950e9
Pi <pchandra@uccs.edu>
2020-07-27 14:26:50 -0600

    verifying kem with bool

--- Working Implementation/kem.py
+++ Working Implementation/kem.py
@@ -7,11 +7,15 @@ print (algs)
 def generate(algorithm):
     kem = oqs.KeyEncapsulation(algorithm)
     public_key = kem.generate_keypair()
+    print ("Generated pub key")
     ciphertext, shared_secret_server = kem.encap_secret(public_key)
+    print ("Set cipher text ")
     shared_secret_client = kem.decap_secret(ciphertext)
     if shared_secret_client == shared_secret_server:
+        print ("In true")
         return True
     else:
+        print ("In false")
         return False
 
 
