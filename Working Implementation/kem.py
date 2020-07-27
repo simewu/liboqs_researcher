8248ceb216e748d27949086423f0a57aa5ed4501
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-07-27 14:22:48 -0600

    correcting errors

--- Working Implementation/kem.py
+++ Working Implementation/kem.py
@@ -9,7 +9,10 @@ def generate(algorithm):
     public_key = kem.generate_keypair()
     ciphertext, shared_secret_server = kem.encap_secret(public_key)
     shared_secret_client = kem.decap_secret(ciphertext)
-    assert shared_secret_client == shared_secret_server
+    if shared_secret_client == shared_secret_server:
+        return True
+    else:
+        return False
 
 
 userAlg = input("Choose PQ algorithm: ")
