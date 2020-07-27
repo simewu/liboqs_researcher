b73cfbbbe0a971eaaeb0fe3ab1cab2eab63b7541
Pi Iyer <54725951+prchander@users.noreply.github.com>
2020-07-27 14:21:09 -0600

    checking KEM

--- Working Implementation/kem.py
+++ Working Implementation/kem.py
@@ -3,3 +3,15 @@ import oqs
 algs = oqs.get_supported_KEM_mechanisms()
 
 print (algs)
+
+def generate(algorithm):
+    kem = oqs.KeyEncapsulation(algorithm)
+    public_key = kem.generate_keypair()
+    ciphertext, shared_secret_server = kem.encap_secret(public_key)
+    shared_secret_client = kem.decap_secret(ciphertext)
+    assert shared_secret_client == shared_secret_server
+
+
+userAlg = input("Choose PQ algorithm: ")
+generate(userAlg)
+
