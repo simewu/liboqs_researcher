5626ec811f7eff04248bd851cf822911b3e1bc47
Pi Iyer <54725951+prchander@users.noreply.github.com>
2022-07-24 17:59:14 -0600

    Update to length

--- Working Implementation/Message length/compile_message_lengths.sh
+++ Working Implementation/Message length/compile_message_lengths.sh
@@ -1,2 +1,9 @@
-g++ -o Message_lengths.o Message_lengths.cpp -L../../build/lib -loqs
-./Message_lengths.o
+
+
+rm -rf Message_lengths.o
+
+# -g for debugging
+g++ -g -o Message_lengths.o Message_lengths.cpp -L../build/lib -loqs
+
+LD_LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH
