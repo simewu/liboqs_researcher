7103f103513b4371b4c2c3d8c752a031040c92e0
Pi Iyer <54725951+prchander@users.noreply.github.com>
2022-07-24 17:40:29 -0600

    Adding compile script

---
+++ compile.sh
@@ -0,0 +1,14 @@
+sudo apt install -y astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml
+
+# For conio.h
+#sudo apt-get install -y libncurses5-dev libncursesw5-dev
+
+rm -rf build
+mkdir build
+cd build
+
+#cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
+#cmake -DLIBOQS_INCLUDE_DIR=/usr/local/include -DLIBOQS_LIB_DIR=/usr/local/lib -DBUILD_SHARED_LIBS=ON -GNinja ..
+cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
+#ninja
+sudo ninja install
