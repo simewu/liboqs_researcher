91a06886973e57d7b37876bcc3cbf5f6d0d63138
Manohar Raavi <raavimanohar5@gmail.com>
2022-09-02 10:17:07 -0600

    pki cert size plot update

--- Working Implementation/Matlab files/Code/journal/pki.m
+++ Working Implementation/Matlab files/Code/journal/pki.m
@@ -1,15 +1,16 @@
 clear all
 
-selection = 1;
+selection = 3;
 % 1 for cert gen
 %2 for cert ver
+%3 for cert size
 
-x = categorical({'RSA 2048','RSA 4096','P256','Dilithium 2','Dilithium 3','Dilithium 5','Falcon 512','Falcon 1024','SPHINCS+Haraka128frobust','SPHINCS+SHA256128frobust','SPHINCS+SHAKE256128frobust'});
-x = reordercats(x,{'RSA 2048','RSA 4096','P256','Dilithium 2','Dilithium 3','Dilithium 5','Falcon 512','Falcon 1024','SPHINCS+Haraka128frobust','SPHINCS+SHA256128frobust','SPHINCS+SHAKE256128frobust'});
+x = categorical({'RSA 2048','RSA 4096','Dilithium 2','Dilithium 3','Dilithium 5','Falcon 512','Falcon 1024','SPHINCS+Haraka128frobust','SPHINCS+SHA256128frobust','SPHINCS+SHAKE256128frobust'});
+x = reordercats(x,{'RSA 2048','RSA 4096','Dilithium 2','Dilithium 3','Dilithium 5','Falcon 512','Falcon 1024','SPHINCS+Haraka128frobust','SPHINCS+SHA256128frobust','SPHINCS+SHAKE256128frobust'});
 
 if selection == 1
     % 1 for cert gen
-    y = [4.74 11.15 4.66 3.84 3.99 4.05 3.97 4.37 11.48 41.58 89.76];
+    y = [4.74 11.15 3.84 3.99 4.05 3.97 4.37 11.48 41.58 89.76];
     bar(x,y)
     ylabel('Certificate Generation Time (ms)','FontSize',18)
     text(1:length(y),y,num2str(y'),'vert','bottom','horiz','center', 'FontSize', 12); 
@@ -18,12 +19,23 @@ if selection == 1
     ylim([0 100])
 elseif selection == 2
     %2 for cert ver
-    y = [2.44 2.63 4.5 3.05 3.59 3.73 3.35 3.53 4.7 10.17 24.28];
+    y = [2.44 2.63 3.05 3.59 3.73 3.35 3.53 4.7 10.17 24.28];
     bar(x,y)
     ylabel('Certificate Verification Time (ms)','FontSize',18)
     %xlabel('Algorithms','FontSize',18)
     text(1:length(y),y,num2str(y'),'vert','bottom','horiz','center', 'FontSize', 12); 
     ylim([0 100])
+elseif selection == 3
+    %3 for cert sizes
+    x = categorical({'RSA 2048','RSA 4096','Dilithium 2','Dilithium 3','Dilithium 5','Falcon 512','Falcon 1024','SPHINCS+128frobust'});
+    x = reordercats(x,{'RSA 2048','RSA 4096','Dilithium 2','Dilithium 3','Dilithium 5','Falcon 512','Falcon 1024','SPHINCS+128frobust'});
+    y = [1000 1696 5340 7391 10020 2358 4426 23442];
+    bar(x,y)
+    ylabel('Certificate Size (Bytes)','FontSize',18)
+    %xlabel('Algorithms','FontSize',18)
+    %set(gca, 'YScale', 'log');
+    text(1:length(y),y,num2str(y'),'vert','bottom','horiz','center', 'FontSize', 12); 
+    ylim([0 25000])
 end
 
     grid on
