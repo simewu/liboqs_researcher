afd1d877a14ecfa6b048ba4884ed4087472a53d6
SimeoW <swuthier@uccs.edu>
2022-11-06 19:49:35 -0700

    Fix compilation error

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+
+
 sudo apt install -y astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml
 
 # For conio.h
@@ -9,6 +11,6 @@ cd build
 
 #cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
 #cmake -DLIBOQS_INCLUDE_DIR=/usr/local/include -DLIBOQS_LIB_DIR=/usr/local/lib -DBUILD_SHARED_LIBS=ON -GNinja ..
-cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
-ninja
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+#ninja
 sudo ninja install
