e066dc3a37597e69f272137e800804bfb988a20f
simewu <swuthier@uccs.edu>
2022-11-14 14:14:48 -0700

    Toggle debugging

--- compile.sh
+++ compile.sh
@@ -11,6 +11,12 @@ cd build
 
 #cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
 #cmake -DLIBOQS_INCLUDE_DIR=/usr/local/include -DLIBOQS_LIB_DIR=/usr/local/lib -DBUILD_SHARED_LIBS=ON -GNinja ..
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+
+# Debugging:
+#cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+
+# No Debugging:
+cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
+
 #ninja
 sudo ninja install
